c8cbb8ba4d93d967606f74451d24b5d1f6be8fbb
Albenoo <albeniskerqeli5@gmail.com>
2020-09-25 01:34:49 +0200

    asdasda

--- app.js
+++ app.js
@@ -1,1 +1,29 @@
-asdadasdas
+let getButtons = document.querySelectorAll('.buy');
+let dropdown = document.querySelector("#menu");
+var adding = 0;
+
+getButtons.forEach(function(btn) {
+  btn.addEventListener('click', function(e) {
+    adding++;
+
+
+    btn.innerHTML = "Check the cart now";
+    let count =  document.querySelector(".number");
+count.innerHTML = adding;
+
+let profile = document.createElement("DIV");
+profile.setAttribute("class" , "product");
+profile.src = "asus.png";
+let image = document.createElement("DIV");
+image.setAttribute("class" , "product-image");
+image.src= "images/msi2.png";
+dropdown.append(profile);
+profile.innerHTML = "Albenis";
+profile.style.display = "flex";
+profile.style.flexDirection = "row";
+profile.append(image);
+
+
+
+  })
+})
